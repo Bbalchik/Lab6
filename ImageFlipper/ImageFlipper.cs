dc39687e1dfc8be78de9ef6c78d8233a5eac3475
balchik11 <nazarpalnnij7@icloud.com>
2025-12-23 21:55:46 +0200

    Detect image files and log results to console

--- ImageFlipper/ImageFlipper.cs
+++ ImageFlipper/ImageFlipper.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            Regex imageExt = new Regex("^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
+
+            foreach (string file in files)
+            {
+                string ext = Path.GetExtension(file).TrimStart('.');
+
+                if (!imageExt.IsMatch(ext))
+                {
+                    Console.WriteLine($"[SKIP] {file} — не картинка");
+                    continue;
+                }
+
+                Console.WriteLine($"[OK]   {file} — КАРТИНКА");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
